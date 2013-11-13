7561f842d7f94ed324c6f496f4e92b71f51e161e
Eva Shek <evashek@hawaii.edu>
2013-11-13 10:49:49 -1000

    Starting WOD

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -20,7 +20,7 @@ public class Application extends Controller {
 
   /**
    * Returns the home page. 
-   * @return The resulting home page. 
+   * @return The resulting home page.
    */
   public static Result index() {
     return ok(Index.render(SurferDB.getSurfers()));
